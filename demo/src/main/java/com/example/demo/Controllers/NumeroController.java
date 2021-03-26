a9b694fbdbcb306cedd40655703b33c3231c639c
AgusBVarela <agustina.bvarela@mercadolibre.com>
2021-03-26 17:08:53 -0300

    Commit Convertir a Numero Romano

---
+++ demo/src/main/java/com/example/demo/Controllers/NumeroController.java
@@ -0,0 +1,24 @@
+package com.example.demo.Controllers;
+
+import com.example.demo.Entities.Numero;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/numero")
+public class NumeroController {
+
+    //Prueba http://localhost:8080/numero/convertir?number=90
+    @GetMapping("/convertir")
+    public Numero convertirARomano(@RequestParam(value = "number", defaultValue = "1") String number) throws Exception{
+
+        int n = Integer.parseInt(number);
+        Numero nObject = new Numero(n);
+        nObject.convertirARomano();
+        return nObject;
+
+
+    }
+}
